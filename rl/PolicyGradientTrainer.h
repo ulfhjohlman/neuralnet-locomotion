33ed564b798df1734663378cdcdff111c0f723e0
Ulf <ulfhjohlman@gmail.com>
2017-10-13 18:11:00 +0200

    tmp save

--- rl/PolicyGradientTrainer.h
+++ rl/PolicyGradientTrainer.h
@@ -9,25 +9,31 @@ public:
     PolicyGradientTrainer(Environment * new_env, NeuralNet * new_policy_net)
             : Trainer(new_env),action_space_dim(env->getActionSpaceDimensions()),
             state_space_dim(env->getStateSpaceDimensions()),
-            policy(PolicyWrapper(new_policy_net,state_space_dim,action_space_dim)) {
+            policy(new_policy_net,state_space_dim,action_space_dim) {
             }
 
-    virtual void train(long max_iterations, long timesteps_per_run, long batch_size)
+    virtual void train(long max_episodes, long timesteps_per_run, long batch_size)
     {
-        reward_list.reserve(batch_size * (timesteps_per_run+1));
-        advantage_list.reserve(batch_size * (timesteps_per_run+1));
+        rew_list.reserve(batch_size * (timesteps_per_run+1));
+        adv_list.reserve(batch_size * (timesteps_per_run+1));
+        ac_list.reserve(batch_size * (timesteps_per_run+1));
+        ob_list.reserve(batch_size * (timesteps_per_run+1));
+        loss_list.reserve(batch_size * (timesteps_per_run+1));
 
-        for(int i=1; i < max_iterations; i++)
+        for(int i=1; i < max_episodes; i++)
         {
-            //if end of an episode
-            if(i % timesteps_per_run == 0)
+            //generate a trajectory (an episode)
+            generateTrajectory(timesteps_per_run);
+
+            //at end of an episode
             {
                 //calculate advFunc
-                //calculate loss
+                calcAdvFunc();
+                calcLoss();
                 //calculate and cache loss gradients
 
                 //if end of a minibatch
-                if(i % (timesteps_per_run*batch_size) == 0)
+                if(i % batch_size == 0)
                 {
                     //pop cache
                     //backprop loss
@@ -36,20 +42,37 @@ public:
                 }
                 //reset stuff
                 env->reset();
-                reward_list.clear();
-                advantage_list.clear();
+                rew_list.clear();
+                adv_list.clear();
+                ac_list.clear();
+                ob_list.clear();
             }
-            //generate trajectories
-            generateTrajectory(timesteps_per_run);
 
         }
     }
 protected:
-    void generateTrajectory(int traj_length)
+    void calcAdvFunc()
     {
-        for(int i=0; i< traj_length;i++)
+        ScalarType decayed_rew = 0;
+        for(int i = ac_list.size()-1; i >= 0; i--)
         {
+            decayed_rew = rew_list[i] + decayed_rew * rew_decay_rate;
+            adv_list[i] = decayed_rew;
+        }
+    }
+
+    void calcLoss()
+    {
+        
+    }
+
+    void generateTrajectory(long traj_length)
+    {
+        for(long i=0; i< traj_length;i++)
+        {
+
             ob = env->getState();
+
             #ifdef _DEBUG
                 if(ob.size() != state_space_dim){
                     char* str = new char[100];
@@ -58,23 +81,34 @@ protected:
                 }
             #endif
 
+            ob_list.push_back(ob);
+            rew_list.push_back(env->getReward());
             ac = policy.samplePolicy(ob);
 
             #ifdef _DEBUG
                 if(ac.size() != action_space_dim){
-                    throw std::runtime_error("Sampled policy != action space size\n");
+                    char* str = new char[100];
+                    sprintf(str,"Sampled policy ()%ld)!= action space size %d\n",ac.size(),action_space_dim);
+                    throw std::runtime_error(str);
                 }
             #endif
+            ac_list.push_back(ac);
+            env->step(ac);
+
+
         }
     }
 
 
-
+    ScalarType rew_decay_rate = 0.99;
     int state_space_dim;
     int action_space_dim;
     PolicyWrapper policy;
     std::vector<ScalarType> ob;
     std::vector<ScalarType> ac;
-    std::vector<ScalarType> advantage_list;
-    std::vector<ScalarType> reward_list;
+    std::vector<ScalarType> adv_list;
+    std::vector<ScalarType> rew_list;
+    std::vector<ScalarType> loss_list;
+    std::vector<std::vector<ScalarType>> ob_list;
+    std::vector<std::vector<ScalarType>> ac_list;
 };
--- rl/PolicyWrapper.h
+++ rl/PolicyWrapper.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "NeuralNet.h"
 #include "stdlib.h"
+#include "Generator.h"
+#include <math.h>
 
 class PolicyWrapper
 {
 public:
-        PolicyWrapper(NeuralNet * new_nn, int new_in_size, int new_out_size): nn(new_nn), in_size(new_in_size) {}
+        PolicyWrapper(NeuralNet * new_nn, int new_in_size, int new_out_size): nn(new_nn), in_size(new_in_size) {
+            sample.reserve(out_size);
+            prob.reserve(out_size);
+        }
 
         //returns a single action_space output from the policy given an observation
         std::vector<ScalarType>& samplePolicy(std::vector<ScalarType> obs)
@@ -13,15 +18,40 @@ public:
             Eigen::Map<MatrixType> in_matrix(obs.data(),in_size,1);
             nn->input(in_matrix);
             out_matrix_ptr = &nn->output();
-            sample = std::vector<ScalarType>(out_matrix_ptr->data(), out_matrix_ptr->data() + out_matrix_ptr->size());
+            mu = std::vector<ScalarType>(out_matrix_ptr->data(), out_matrix_ptr->data() + out_matrix_ptr->size());
+            sample.clear();
+            prob.clear();
+            ScalarType x;
+            for(int i=0;i<mu.size();i++)
+            {
+                //with sigma = 1 for all variables; (for now)
+                x = generator.generate_normal<ScalarType>(mu[i],1);
+                sample.push_back(x);
+                //store probability of the sample in 'prob'
+                prob.push_back(norm_pdf(x,mu[i],1));
+            }
             return sample;
         }
+
+        //returns the cumulative probability of the sample TODO: BETTER
+        double getCumulativeProb()
+        {
+
+        }
+
 private:
+        ScalarType norm_pdf( ScalarType x , mu , sigma)
+        {
+            return 1.0/(sigma*sqrt(2*M_PI)) * exp(-(x-mu)*(x-mu)/(2.0*sigma*sigma))
+        }
+
         NeuralNet * nn;
         std::vector<ScalarType> sample;
+        std::vector<ScalarType> prob;
+        std::vector<ScalarType> mu;
         MatrixType in_matrix;
         const MatrixType * out_matrix_ptr = nullptr;
         int in_size;
         int out_size;
-
+        Generator generator; //Thread safe generation
 };
